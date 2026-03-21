1bd4171e5c080171816fc72fc6b5e4ac054fdbd8
mgpacifique <ovouzzy@gmail.com>
2026-03-21 02:50:10 +0200

    Refactor command in user limit Puppet script to simplify file limit updates

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # Increases the open file limit for the holberton user
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 50000/" /etc/security/limits.conf && sed -i "s/holberton soft nofile 4/holberton soft nofile 50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton hard nofile/s/[0-9]*/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/:/usr/bin/',
 }
